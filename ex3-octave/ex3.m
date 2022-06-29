7232d9b7fc61f9a2ab70f5aab60958cfd53310e3
andaolong <1725312729@qq.com>
2022-06-29 10:51:01 +0800

    完成了ex3-octave部分的第一部分：多分类逻辑回归。 (完成了oneVsAll函数和predictOneVsAll函数) 2022年6月29日10:50:51：

--- ex3-octave/ex3.m
+++ ex3-octave/ex3.m
@@ -125,14 +125,37 @@ fprintf(' 0.146561\n -0.548558\n 0.724722\n 1.398003\n');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+
+
+
+
+
+
+
+
 %% ============ Part 2b: One-vs-All Training ============
-fprintf('\nTraining One-vs-All Logistic Regression...\n')
+fprintf('\nTraining One-vs-All Logistic Regression...\n');
 
 lambda = 0.1;
 [all_theta] = oneVsAll(X, y, num_labels, lambda);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 %% ================ Part 3: Predict for One-Vs-All ================
 
